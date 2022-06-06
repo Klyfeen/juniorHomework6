21d577aa5aebedf724f2225fc1eb053bde270816
Klyfeen <mlgggeasy@gmail.com>
2022-06-06 09:47:18 +0500

    Add project files.

---
+++ juniorHomework6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juniorHomework6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int albumPictures = 52;
+            int specialZone = 3;
+            int includedPictures = albumPictures / specialZone;
+            int unincludedPictures = albumPictures % specialZone;
+
+            Console.WriteLine($"Полностью заполненных рядов: {includedPictures}");
+            Console.WriteLine($"Количество картинок сверх меры: {unincludedPictures}");
+        }
+    }
+}
